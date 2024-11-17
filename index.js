17b286e4eeb03fb3dc57602e4d53b7b3cbde5363
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2024-11-17 22:49:00 +0600

    Learning Node

---
+++ index.js
@@ -0,0 +1,12 @@
+//lcoal modules
+
+const { add } = require('./local-1');
+const { add: add2 } = require('./local-2')
+// console.log(add(2, 3))
+// console.log(add2(2, 3, 5))
+
+//build-in modules
+const path = require("path");
+console.log(path.join("/F:/Mission 2/larning-node/","local-1.js"))
+
+
